d0ec7e6739e69d8cb44c7807b9838afec3bdb520
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2025-09-27 00:28:17 +0530

    feat: create POST /signup API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server listening on PORT 3000...");
-})
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User added successfully!");
+  } catch (err) {
+    res.status(400).send("Error adding user:" + err.message);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully!");
+    app.listen(3000, () => {
+      console.log("Server listening on PORT 3000...");
+    });
+  })
+  .catch((err) => console.log("Failed to connect to the database!"));
