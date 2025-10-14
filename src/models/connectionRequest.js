4f44ea120e34a25dc6a39fa4f7eeb25fb27e875e
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2025-10-15 01:00:40 +0530

    feat: connectionRequest Schema

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["IGNORED", "INTERESTED", "ACCEPTED", "REJECTED"],
+        message: ` {VALUE} is not a valid status`,
+      },
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+connectionRequestSchema.index({ senderId: 1, receiverId: 1 });
+
+connectionRequestSchema.pre("save", function (next) {
+  let connectionRequest = this;
+  // connectionRequest is an object created using connectionRequestModel
+  if (connectionRequest.senderId.equals(connectionRequest.receiverId)) {
+    return next(new Error("Cannot send a connection request to yourself"));
+  }
+  next();
+});
+
+const ConnectionRequestModel = mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = ConnectionRequestModel;
