ab3b2169bedd206bb5dc1f69a120ecf5309c9a1b
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2025-10-15 01:00:04 +0530

    feat: logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -50,4 +50,9 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/logout", async (req, res) => {
+  res.cookie("token", null, { expires: new Date(Date.now()) });
+  res.send("Logout successful!");
+});
+
 module.exports = router;
