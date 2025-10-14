20ff8deee9b81be4e786917c211c195b56889486
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2025-10-15 01:01:13 +0530

    feat: API to edit the profile and reset the password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-router.get("/profile", userAuth, async (req, res) => {
+router.get("/profile/view", userAuth, async (req, res) => {
   try {
     res.send(req.user);
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+});
+
+router.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Update not allowed");
+    }
+
+    let loggedInUser = req.user;
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
+  }
+});
+
+router.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    let { oldPassword, newPassword } = req.body;
+    let user = req.user;
+
+    let isPasswordValid = await user.validatePassword(oldPassword);
+    if (!isPasswordValid) {
+      throw new Error("Invalid Password");
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error("Password is not strong enough!");
+    }
+
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+    user.password = passwordHash;
+    await user.save();
+    res.send("Password updated successfully");
   } catch (error) {
     res.status(400).send("ERROR: " + error.message);
   }
