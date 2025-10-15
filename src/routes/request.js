2acbdf4e8fa86117530c5f70405df96eaf864396
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2025-10-16 04:22:26 +0530

    feat: API to accept/reject a Connection Request

--- src/routes/request.js
+++ src/routes/request.js
@@ -73,8 +73,6 @@ router.post("/requests", userAuth, async (req, res) => {
       status,
     });
 
-    console.log("Connection Request: ", connectionRequest);
-
     const data = await connectionRequest.save();
     let message;
     switch (status) {
@@ -98,6 +96,94 @@ router.post("/requests", userAuth, async (req, res) => {
       message: error.message || "Internal Server Error",
       data: null,
       errors: { general: error.message || "Internal Server Error" },
+    });
+  }
+});
+
+router.patch("/requests/:requestId", userAuth, async (req, res) => {
+  try {
+    const { status } = req.body;
+    const requestId = req.params.requestId;
+
+    // validation for status
+    if (!["ACCEPTED", "REJECTED"].includes(status)) {
+      return res.status(400).json({
+        status: "error",
+        message: "Invalid status type",
+        data: null,
+        errors: { status: `Status must be either 'ACCEPTED' or 'REJECTED'` },
+      });
+    }
+
+    const connectionRequest = await ConnectionRequestModel.findById(requestId);
+    // validation to check if connectionRequest exists
+    if (!connectionRequest) {
+      return res.status(404).json({
+        status: "error",
+        message: "Connection request not found",
+        data: null,
+        errors: {
+          requestId: "No connection request found with the provided requestId",
+        },
+      });
+    }
+
+    // validation to check if the logged in user is the receiver of the connection request
+    const loggedInUserId = req.user["_id"];
+    if (!connectionRequest.receiverId.equals(loggedInUserId)) {
+      return res.status(403).json({
+        status: "error",
+        message:
+          "Unauthorized access: You can only modify your own connection requests",
+        data: null,
+        errors: {
+          authorization: "You do not have permission to modify this request",
+        },
+      });
+    }
+
+    // validation to check if the connection request was already IGNORED
+    if (connectionRequest.status === "IGNORED") {
+      return res.status(409).json({
+        status: "error",
+        message: "Profile already ignored",
+        data: null,
+        errors: {
+          status:
+            "The connection request has already been ignored and cannot be processed again.",
+        },
+      });
+    }
+
+    // validation to check if the connection request was already ACCEPTED or REJECTED
+    if (["ACCEPTED", "REJECTED"].includes(connectionRequest.status)) {
+      return res.status(409).json({
+        status: "error",
+        message: `Connection request already ${connectionRequest.status.toLowerCase()}`,
+        data: null,
+        errors: {
+          status: `The connection request has already been ${connectionRequest.status.toLowerCase()} and cannot be processed again.`,
+        },
+      });
+    }
+
+    connectionRequest.status = status;
+    const data = await connectionRequest.save();
+
+    res.status(200).json({
+      status: "success",
+      message: `Connection request is ${status.toLowerCase()}`,
+      data,
+      errors: null,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message || "Internal server error",
+      data: null,
+      errors: {
+        general: error.message || "Internal server error",
+      },
     });
   }
 });
