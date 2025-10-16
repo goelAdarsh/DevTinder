d4a6c5d0dcd6478becf7d75981eba5bf77c7250a
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2025-10-16 20:23:02 +0530

    feat: Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
-const ConnectionRequestModel = require("../models/connectionRequest");
+const { ConnectionRequestModel } = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -8,7 +9,8 @@ const USER_SELECT_FIELDS = [
   "firstName",
   "lastName",
   "age",
-  "photoURL",
+  "about",
+  "photoUrl",
   "skills",
 ];
 
@@ -53,7 +55,7 @@ router.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedInUserId = req.user["_id"];
 
-    const data = await ConnectionRequestModel.find({
+    let data = await ConnectionRequestModel.find({
       $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
       status: "ACCEPTED",
     }).populate({
@@ -70,9 +72,68 @@ router.get("/user/connections", userAuth, async (req, res) => {
       });
     }
 
+    data = data.map((connection) => {
+      if (connection.senderId["_id"].equals(loggedInUserId)) {
+        return connection.receiverId;
+        // receiverId contains the entire user document of the receiver (but only selected
+        // fields)
+      } else {
+        return connection.senderId;
+      }
+    });
+
     res.status(200).json({
       status: "success",
       message: "Connections fetched successfully",
+      data,
+      errors: null,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message || "Internal Server Error",
+      data: null,
+      errors: { general: error.message || "Internal Server Error" },
+    });
+  }
+});
+
+router.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUserId = req.user["_id"];
+
+    // fetch all connection requests (irrespective of status)
+    const processedConnectionRequests = await ConnectionRequestModel.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    }).select(["senderId", "receiverId"]);
+
+    // hideUsersList contains
+    // 1. ids of all the users who has interacted with the loggedInUser
+    // 2. id of the loggedInUser
+    const hideUsersList = new Set();
+    processedConnectionRequests
+      ? processedConnectionRequests.forEach((connectionRequest) => {
+          hideUsersList.add(connectionRequest.receiverId.toString());
+          hideUsersList.add(connectionRequest.senderId.toString());
+        })
+      : hideUsersList.add(loggedInUserId.toString());
+
+    const data = await User.find({
+      _id: { $nin: [...hideUsersList] },
+    }).select(USER_SELECT_FIELDS);
+
+    if (!data.length) {
+      return res.status(200).json({
+        status: "success",
+        message: "No other profiles found",
+        data: [],
+        errors: null,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "User feed fetched successfully",
       data,
       errors: null,
     });
